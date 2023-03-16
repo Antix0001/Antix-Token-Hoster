ad3696b72b6360fd6448f768861c8d633b87d869
Antix <126834613+Antix0001@users.noreply.github.com>
2023-03-16 06:39:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,28 @@
 from keep_alive import keep_alive
-import requests, sys, time, os, json
+import os
+os.system("pip install yaml")
+import discord, json, requests, yaml
+from discord.ext import commands
 from colored import fg, attr
-from colorama import Fore, Style
-
-############################################
-token = "your-token"
-status = "Hey this id is hosted"
-emoji = "emoji" 
-emoji_id = "<emoji-id>"
-############################################
 
 class colors:
   pink = fg('#FF4DE1')
   gray = fg('#E6F0F5')
   redd = fg('#FF0051')
   blue = fg('#6CFE00')
-headers = {"Authorization": token, "content-type": "application/json"}
+######################################
+config = yaml.safe_load(open("config.yml"))["settings"]
+tkn = os.environ['tkn'] #keep's your token safe
+r = config['status']
+i = config['emoji']
+######################################
+client = commands.Bot(command_prefix="Antix", self_bot=True)
+payload = {"text": r,"emoji_name": i}
+headers = {"Authorization":tkn,"content-type":"application/json"}
 
-print(f'''
+@client.event
+async def on_ready():
+   print(f'''
 {colors.redd}  █████╗  ███╗  ██╗ ████████╗ ██╗  ██╗  ██╗
 {colors.gray} ██╔══██╗ ████╗ ██║ ╚══██╔══╝ ██║  ╚██╗██╔╝
 {colors.redd} ███████║ ██╔██╗██║    ██║    ██║   ╚███╔╝
@@ -25,34 +30,10 @@ print(f'''
 {colors.redd} ██║  ██║ ██║ ╚███║    ██║    ██║  ██╔╝╚██╗
 {colors.gray} ╚═╝  ╚═╝ ╚═╝  ╚══╝    ╚═╝    ╚═╝  ╚═╝  ╚═╝ 
 {colors.pink}[>] WELCOME TO ANTIX HOSTER
-{colors.pink}[>] Your ID is Hosted
+{colors.pink}[>] Connected as : {client.user}"
 {colors.pink}[>] Made by Antix#0013{colors.gray}
 ''')
-
-class antix:
-  def status(self):
-    r = requests.patch("https://discord.com/api/v9/users/@me/settings",
-                       headers=headers,
-                       json={
-                         "status": "idle",
-                         "custom_status": {
-                           "text": status,
-                           "emoji_name": emoji,
-                           "emoji_id": int(emoji_id)
-                         }
-                       })
-    if r.status_code == 401:
-      print("Invalid token")
-      sys.exit()
-    elif r.status_code == 429:
-      print("Too many requests")
-      time.sleep(10)
-      sys.exit()
-    else:
-      pass
-
-  def start(self):
-    self.status()
-    antix().start()
-keep_alive()
-antix().start()
+   requests.patch("https://discord.com/api/v9/users/@me/settings",headers=headers,json={"status": "idle","custom_status": payload})
+  
+keep_alive()  
+client.run(tkn)
